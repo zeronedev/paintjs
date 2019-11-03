c80f8b85189bb1995497fd08e87da55805753da8
zeronedev <ok7375@gmail.com>
2019-11-03 21:25:47 +0900

    2.2 Recap! done

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strockeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,22 +13,28 @@ function stopPainting() {
   painting = false;
 }
 
-function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
   painting = true;
 }
 
-function onMouseUp(event) {
-  painting = false;
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!painting) {
+    console.log("creating path in ", x, y);
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    console.log("creating line in ", x, y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
