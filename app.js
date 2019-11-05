1409704f6c7b163b840b7d4101590a2d8cc319f9
zeronedev <ok7375@gmail.com>
2019-11-05 21:34:49 +0900

    2.3 Changing Color done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 500;
 canvas.height = 500;
 
-ctx.strockeStyle = "#2c2c2c";
+ctx.strokeStyle = "#2C2C2C";
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -22,11 +23,11 @@ function onMouseMove(event) {
   const y = event.offsetY;
 
   if (!painting) {
-    console.log("creating path in ", x, y);
+    //console.log("creating path in ", x, y);
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
-    console.log("creating line in ", x, y);
+    //console.log("creating line in ", x, y);
     ctx.lineTo(x, y);
     ctx.stroke();
   }
@@ -38,3 +39,14 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+function handleColorClick(event) {
+  //console.log(event.target.style.backgroundColor);
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+//console.log(Array.from(colors));
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);
