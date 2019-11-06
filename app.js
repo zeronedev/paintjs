1914011c1c8b3ca52afb151785632321cc3ebb72
zeronedev <ok7375@gmail.com>
2019-11-06 21:22:56 +0900

    2.5 Filling Mode done

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-canvas.width = 500;
-canvas.height = 500;
+const INITIAL_COLOR = "#2C2C2C";
+const CANVAS_SIZE = 500;
 
-ctx.strokeStyle = "#2C2C2C";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -40,6 +44,7 @@ function handleColorClick(event) {
   //console.log(event.target.style.backgroundColor);
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -58,11 +63,18 @@ function handelModeChange() {
   }
 }
 
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 //console.log(Array.from(colors));
